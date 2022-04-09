761ca6adec6d55ca5a095435186787a086faa731
pahalagedara <thilinam4@gmail.com>
2022-04-10 00:09:05 +0530

    change UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,40 +7,44 @@ import {
   faTwitter,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <div className='footer-top '>
-        <div className='footer-center col-lg-6'>
-          <p>©2021 CODEPIRATEZ</p>
+      <Container fluid={false}>
+        <div className='footer-top '>
+          <div className='footer-copyright col-lg-6'>
+            <p>©2021 CODEPIRATEZ</p>
+          </div>
+          <div className=' col-lg-6'>
+            <img src={logo} alt={"medec"} />
+          </div>
         </div>
-        <div className='footer-center col-lg-6'>
-          <img src={logo} alt={"medec"} />
+        <hr />
+        <div className='footer-bottom'>
+          <div className='col-lg-4 footer-navlink footer-link'>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+          </div>
+          <div className='col-lg-4'></div>
+          <div className='col-lg-4 footer-social-media footer-link'>
+            <a href='#'>
+              <FontAwesomeIcon icon={faFacebook} size='1x' color='black' />
+            </a>
+            <a href='#'>
+              <FontAwesomeIcon icon={faTwitter} size='1x' color='black' />
+            </a>
+            <a href='#'>
+              <FontAwesomeIcon icon={faYoutube} size='1x' color='black' />
+            </a>
+            <a href='#'>
+              <FontAwesomeIcon icon={faInstagram} size='1x' color='black' />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className='footer-bottom'>
-        <div className='col-lg-4 footer-navlink footer-link'>
-          <a href='#'>Home</a>
-          <a href='#'>About</a>
-          <a href='#'>Contact</a>
-        </div>
-        <div className='col-lg-4'></div>
-        <div className='col-lg-4 footer-social-media footer-link'>
-          <a href='#'>
-            <FontAwesomeIcon icon={faFacebook} size='1x' color='black' />
-          </a>
-          <a href='#'>
-            <FontAwesomeIcon icon={faTwitter} size='1x' color='black' />
-          </a>
-          <a href='#'>
-            <FontAwesomeIcon icon={faYoutube} size='1x' color='black' />
-          </a>
-          <a href='#'>
-            <FontAwesomeIcon icon={faInstagram} size='1x' color='black' />
-          </a>
-        </div>
-      </div>
+      </Container>
     </div>
   );
 };
