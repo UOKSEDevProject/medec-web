2a3aa351c4c8b366c9e3f87b1d1ba4fdac6c8882
Chanaka-Madushan-Herath <chanaka.herath.1998@gmail.com>
2022-04-14 11:19:54 +0530

    reformat fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
-import DctList from "./body/DctList";
+import DctList from "./Body/DctList";
 import { Container } from "react-bootstrap";
-import Search from "./body/Search";
-import DoctorProfile from "./body/DoctorProfile";
+import Search from "./Body/Search";
+import DoctorProfile from "./Body/DoctorProfile";
 import PatientProfile from './Body/PatientProfile';
 
 const Body = () => {
@@ -11,7 +11,7 @@ const Body = () => {
         <Search />
         <DctList />
         <DoctorProfile />
-          <PatientProfile/>
+        <PatientProfile />
       </Container>
     </div>
   );
