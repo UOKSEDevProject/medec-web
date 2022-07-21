fdb5deb346b6acaa41a2d4ac3f1632e14f4baac1
CharithNiroshan <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>
2022-07-22 04:48:59 +0530

    Completed the Add Prescription UI.

--- src/const/const.js
+++ src/const/const.js
@@ -1,4 +1,4 @@
-export const bloodGroups =[
+export const bloodGroups = [
     {bloodGroup: 'A+'},
     {bloodGroup: 'A-'},
     {bloodGroup: 'B+'},
@@ -8,37 +8,37 @@ export const bloodGroups =[
     {bloodGroup: 'O+'},
     {bloodGroup: 'O-'},
 ]
-export const tittle =[
+export const tittle = [
     {tittle: 'Mr'},
     {tittle: 'Mrs'},
     {tittle: 'Ms'},
     {tittle: 'Hon'},
 ]
 
-export const CHECKOUT_FIELDS ={
+export const CHECKOUT_FIELDS = {
     FIRST_NAME: 'firstName',
     LAST_NAME: 'lastName',
     ADDRESS: 'address',
     PHONE_NUMBER: 'phoneNumber',
     Full_Name: 'fullName',
     Email: 'email',
-    Gender:'gender',
-    Specialization:'specialization',
-    MedicalCouncilNumber:'medicalCouncilNumber',
-    Title:'tittle',
-    Birthdate:'birthDate',
-    Country:'country',
-    BloodGroup:'bloodGroup',
+    Gender: 'gender',
+    Specialization: 'specialization',
+    MedicalCouncilNumber: 'medicalCouncilNumber',
+    Title: 'tittle',
+    Birthdate: 'birthDate',
+    Country: 'country',
+    BloodGroup: 'bloodGroup',
 }
 
 export const genders = [
-    {gender:'Male'},
-    {gender:'Female'},
+    {gender: 'Male'},
+    {gender: 'Female'},
 ]
 
-export const specializations =[
-    {specialization:'General Practitioner'},
-    {specialization:'Cardiologist'}
+export const specializations = [
+    {specialization: 'General Practitioner'},
+    {specialization: 'Cardiologist'}
 ]
 
 export const component = {
@@ -46,3 +46,47 @@ export const component = {
     laboratory: 'LABORATORY',
     user: 'USER'
 }
+
+export const addPrescriptionsSections = [
+    {
+        title: "Laboratory Investigations",
+        options: [
+            "FBS",
+            "PPBS",
+            "RBS",
+            "Urine HCG",
+            "Trop 1",
+            "FBC",
+            "Urine Culture/ABST",
+            "Dengue Antigen",
+            "ALT/SGPT",
+            "Dengue Antibody",
+            "ESR",
+            "Gamma GT",
+            "Urine Micro Albumin",
+            "Lipid Profile",
+            "Liver Profile",
+            "CRP",
+        ]
+    },
+    {
+        title: "MRI/CT Requisitions",
+        options: [
+            "Brain",
+            "Brain MRI",
+            "Whole Spine",
+            "C/Spine",
+            "D/Spine",
+            "L/Spine",
+            "Backache Protocol",
+            "Whole Body",
+            "Carotid Angiogram",
+            "Arotic Angiogram",
+            "MRI Uro",
+            "CT Uro",
+            "MRI Wrist",
+            "MRI Hand",
+            "MRI Knee",
+        ]
+    }
+]
