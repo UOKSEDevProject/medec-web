5a3798fbbc9160435b5fd1a70c6a8db3780287f8
CharithNiroshan <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>
2023-01-31 00:20:54 +0530

    Added role based channel button functionality

--- src/components/body/Session.jsx
+++ src/components/body/Session.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
 import {configuration} from "../../config";
-import {component} from "../../constants/constants";
+import {useHistory} from "react-router-dom";
 
 function Session(props) {
+    const history = useHistory();
+
+    const onClick = () => {
+        switch (configuration.component) {
+            case "CHAN_CENTER": {
+                history.push("/add-mnl-app");
+                break;
+            }
+            case "DOCTOR": {
+                history.push("/pnt-lst");
+                break;
+            }
+            case "USER": {
+                if (sessionStorage.key(0)) {
+                    console.log("test")
+                } else {
+                    history.push("/login");
+                }
+                break;
+            }
+            default: {
+                history.push("/login");
+                break;
+            }
+        }
+    }
+
     return (
         <div className='session-item-body'>
             <div>{props.date}</div>
             <div>{props.time}</div>
             <div>
                 <div className='session-appointment-name'>Appointments</div>
-                <div className={props.maximumAppointments===props.appointments?'max-appointments':'lower-appointments'}>
+                <div
+                    className={props.maximumAppointments === props.appointments ? 'max-appointments' : 'lower-appointments'}>
                     {props.appointments}
                 </div>
             </div>
             <Button className='session-view-btn'
+                    onClick={onClick}
                     disabled={
-                        configuration.component === 'DOCTOR' ? false : props.maximumAppointments===props.appointments}
+                        configuration.component === 'DOCTOR' ? false : props.maximumAppointments === props.appointments}
             >
                 {props.text}</Button>
         </div>
