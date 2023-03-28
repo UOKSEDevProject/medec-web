e004640a9023b44a6e95bdaa624b0f4ce2d78e30
Thilina Madushan <thilinam4@gmail.com>
2023-03-28 18:06:39 +0530

    fixed integration issue

--- src/components/body/MedicalHistory.jsx
+++ src/components/body/MedicalHistory.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import CollapseView from "./list view/CollapseView";
+import TestCollapseView from "./list view/CollapseView";
 import store from "../../data-store/reducer/root-reducer";
 import {patientActions} from "../../data-store/actions/patient-actions";
 import {useQuery} from "@apollo/client";
@@ -7,13 +7,6 @@ import queries from "../../graphql/queries";
 import {useSelector} from "react-redux";
 import Spinner from "./Spinner";
 import {useParams} from "react-router-dom";
-
-let screenHeight = window.screen.height;
-let screenWidth = window.screen.width;
-//todo change height the colaps and image
-let collapseHeight = screenHeight / 0.7;
-let collapseWidth = screenWidth / 3;
-let prescriptionImageHeight = collapseHeight;
 
 const addMedicalHistoryToStore = (data) => {
     store.dispatch(patientActions.addMedicalHistory(data.getMedicalReportList.payload))
@@ -28,78 +21,51 @@ const MedicalHistory = () => {
         }
     });
     const medicalHistoryList = useSelector(state => state.patientDS.medicalHistoryList);
-    const [medHis, setMedHis] = useState(undefined);
     const [isShowCollapesView, setIsShowCollapesView] = useState(true);
     const [selectedItem, setSelectedItem] = useState(undefined);
 
     useEffect(() => {
-        if (medicalHistoryList) {
-            setMedHis()
-            setSelectedItem(0);
-            // setSelectedItem(medicalHistoryList[0]?.reports[0]);
+        //select first report
+        if (medicalHistoryList && medicalHistoryList.length>0) {
+            for(let i=0; i<medicalHistoryList.length; i++){
+                let item = medicalHistoryList[i];
+                if(item.reports && item.reports.length>0){
+                    for(let j=0; j<item.reports.length; j++){
+                        if(item.reports[j]){
+                            setSelectedItem(item.reports[j]);
+                            i=medicalHistoryList.length;
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }, [medicalHistoryList])
 
-    const drawerWidthHandler = () => {
-        let isMobile = false;
-        let width = isMobile
-            ? isShowCollapesView
-                ? screenWidth
-                : 50
-            : isShowCollapesView
-                ? collapseWidth
-                : 50;
-        return width;
-    };
-
-    return loading ? <Spinner isOverLay={true}/> :
-        <div className='row'>
-            {/* --------------------------list view------------------------- */}
-
-            <div
-                className='report-list-collapse-container'
-                style={{
-                    backgroundColor: !isShowCollapesView && "rgba(0,0,0,0)",
-                    width: drawerWidthHandler(),
-                }}
-            >
-                <CollapseView
-                    // title={"Medical History"}
-                    // listData={medHis && medHis}
-                    // setIsShowCollapesView={setIsShowCollapesView}
-                    // isShowCollapesView={isShowCollapesView}
-                    // collapseHeight={collapseHeight}
-                    // setSelectedItem={setSelectedItem}
-                    // selectedItem={selectedItem && selectedItem}
-
-                    title={"Medical History"}
-                    items={medicalHistoryList}
-                    index={selectedItem && selectedItem}
-                    setIndex={setSelectedItem}
-                    isCollapsed={isShowCollapesView}
-                    setIsCollapsed={setIsShowCollapesView}
-                />
+    return (
+        loading ? <Spinner isOverLay={true}/> :
+            <div className="lab-report-portal">
+                <div className="pt-container d-flex lab-container" id={isShowCollapesView ? "pt-container-overlay" : "empty"}>
+                    <TestCollapseView
+                        title={"Lab Report List"}
+                        items={medicalHistoryList}
+                        index={selectedItem}
+                        setIndex={setSelectedItem}
+                        isCollapsed={isShowCollapesView}
+                        setIsCollapsed={setIsShowCollapesView}
+                    />
+                    <div className="pt flex-grow-1 presc-container">
+                        {selectedItem && typeof(selectedItem.imgUrl)!=='undefined' &&
+                            <img
+                                src={selectedItem.imgUrl}
+                                //  style={{width:"100%",objectFit:"contain"}}
+                                loading='lazy'
+                                alt="prescription"
+                            />}
+                    </div>
+                </div>
             </div>
-
-            {/*---------------------------------------------------------------*/}
-
-            <div
-                style={{
-                    margin: "20px 0",
-                    height: prescriptionImageHeight,
-                }}
-            >
-                {/*<center>*/}
-                {/*    <img*/}
-                {/*        src={medicalHistoryList[selectedItem]?.imgURL}*/}
-                {/*        loading='lazy'*/}
-                {/*        height={prescriptionImageHeight}*/}
-                {/*        width={"auto"}*/}
-                {/*        alt={"prescription"}*/}
-                {/*    />*/}
-                {/*</center>*/}
-            </div>
-        </div>;
+        );
 };
 
 export default MedicalHistory;
